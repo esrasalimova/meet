6727679ede62959fca948dc49bc9bd08432d9c0f
esrasalimova <82524635+esrasalimova@users.noreply.github.com>
2021-08-01 21:00:53 +0200

    fixes on test files

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,57 +1,52 @@
-import React, { Component } from 'react';
+    import React, { Component } from 'react';
 
-class CitySearch extends Component {
-  state = {
+    class CitySearch extends Component {
+    state = {
     locations: this.props.locations,
     query: '',
-    suggestions: [],
-    showSuggestions: undefined
-  }
-  handleInputChanged = (event) => {
+    suggestions: []
+    }
+
+    handleInputChanged = (event) => {
     const value = event.target.value;
     const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    }); 
+    this.setState({ 
+        query: value,
+        suggestions,
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
-  };
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion,
-      suggestions: [],
-      showSuggestions: false
-    });
+    };
 
-    this.props.updateEvents(suggestion);
-  }
-  render() {
-    return (
-      <div className="CitySearch">
-        <p>Search your city</p>
-        <input type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-          onFocus={() => { this.setState({ showSuggestions: true }) }} />
-        <div>
-          <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
-            {this.state.suggestions.map((suggestion) => (
-              <li
-                key={suggestion}
-                onClick={() => this.handleItemClicked(suggestion)}
-              >{suggestion}</li>
+    handleItemClicked = (suggestion) => {
+    this.setState({
+        query: suggestion
+    });
+    }
+
+    render() {
+        return (
+        <div className="CitySearch">
+            <input
+                type="text"
+                className="city"
+                value={this.state.query}
+                onChange={this.handleInputChanged}
+                />
+                <ul className="suggestions">
+                {this.state.suggestions.map((suggestion) => (
+                    <li 
+                    key={suggestion}
+                    onClick={() => this.handleItemClicked(suggestion)}
+                    >{suggestion}</li>
             ))}
-            <li onClick={() => this.handleItemClicked("all")}>
-              <b>See all cities</b>
+            <li key='all'>
+                <b>See all cities</b>
             </li>
-          </ul>
+            </ul>
         </div>
-      </div>
-    );
-  }
-}
+        );
+    }
+    }
 
-export default CitySearch;
-
+    export default CitySearch;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,12 +4,10 @@ import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 import NumberOfEvents from '../NumberofEvents';
-
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure ({ adapter: new Adapter() });
-
 
 describe('<App /> component', () => {
   let AppWrapper;
@@ -25,7 +23,7 @@ describe('<App /> component', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 
-  test('renders the NumberofEvents', () => {
-    expect(AppWrapper.find(NumberofEvents)).toHaveLength(1);
+  test('render NumberOfEvents correctly', () => {
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
